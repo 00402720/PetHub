df6ea73304bda6139dac70a5ad0d0c1085ae5b17
00402720 <00402720@uca.edu.sv>
2022-11-29 09:30:35 -0600

    Rollback: Unexpected result, return to previous version

--- pethub-app-client/src/App.jsx
+++ pethub-app-client/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <Routes>
       <Route exact path = "/sign-up" element = {<SignUp />} />
       <Route exact path = "/" element = {<MainPage />} />
-      <Route path = "/Profile" element = {<ProfilePage />} />
+      <Route exact path = "/Profile" element = {<ProfilePage />} />
       <Route exact path = "/login" element = {<LoginPage />} />
     </Routes>
   )
